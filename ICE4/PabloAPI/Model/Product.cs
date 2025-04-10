d3c3823160006feb1adf27bab8b9b085f9c42f7f
unknown <monte@ekronds.co.za>
2025-04-10 10:31:43 +0200

    IMplemented ef core and created product controller

---
+++ ICE4/PabloAPI/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PabloAPI.Model
+{
+    public class Product
+    {
+        [Key] 
+        public int id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+    }
+}
